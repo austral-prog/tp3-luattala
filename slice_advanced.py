60a1dd2f47eb0ac962be6e908709785cd894f8c5
luattala <lattala@mail.austral.edu.ar>
2025-08-25 11:45:45 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,5 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+	texto = input()
+	print(texto[4::2])
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+slice_advanced()
